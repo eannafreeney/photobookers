061eaa14d044e8a317f33501ccd943fc1b19645c
Eanna Freeney <eanna@kptncook.com>
2026-02-15 22:06:27 +0100

    v2

--- src/components/app/WishlistedBooks.tsx
+++ src/components/app/WishlistedBooks.tsx
@@ -11,23 +11,28 @@ type WishlistedBooksProps = {
 const WishlistedBooks = async ({ user }: WishlistedBooksProps) => {
   const wishlistBooks = await getBooksInWishlist(user.id);
 
+  if (!wishlistBooks || wishlistBooks?.length === 0) {
+    return (
+      <div
+        id="empty-wishlist-books"
+        class="flex flex-col gap-4 items-center justify-center"
+      >
+        Start adding books to your wishlist and collection to see them here.
+      </div>
+    );
+  }
+
+  const attrs = {
+    "x-init": true,
+    "x-on:wishlist:updated.window":
+      "$ajax('/api/wishlist-books', { target: ['wishlist-books-grid', 'empty-wishlist-books'], sync: true })",
+  };
+
   return (
-    <GridPanel
-      id="wishlist-books-grid"
-      isFullWidth
-      {...{
-        "x-init": true,
-        "x-on:wishlist:updated.window":
-          "$ajax('/api/wishlist-books', { target: 'wishlist-books-grid', sync: true })",
-      }}
-    >
-      {wishlistBooks && wishlistBooks?.length > 0 ? (
-        wishlistBooks?.map((book) => <BookCard book={book} user={user} />)
-      ) : (
-        <div class="flex flex-col gap-4 items-center justify-center">
-          Start adding books to your wishlist and collection to see them here.
-        </div>
-      )}
+    <GridPanel id="wishlist-books-grid" isFullWidth {...attrs}>
+      {wishlistBooks?.map((book) => (
+        <BookCard book={book} user={user} />
+      ))}
     </GridPanel>
   );
 };
