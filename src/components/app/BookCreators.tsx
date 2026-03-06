eb7eca328d0e3961566cc760db135b7fa395ce5c
Eanna Freeney <eanna@kptncook.com>
2026-03-06 21:53:35 +0100

    removed publisher

--- src/components/app/BookCreators.tsx
+++ src/components/app/BookCreators.tsx
@@ -4,15 +4,23 @@ import { BookCardResult } from "../../constants/queries";
 type Props = {
   book: BookCardResult;
   currentCreatorId?: string | null;
+  showPublisher?: boolean;
 };
 
-const BookCreators = ({ book, currentCreatorId }: Props) => {
+const BookCreators = ({
+  book,
+  currentCreatorId,
+  showPublisher = false,
+}: Props) => {
   return (
     <div class="flex items-center justify-between gap-2">
-      <CardCreatorCard creator={book.artist ?? null} />
       {(!currentCreatorId || currentCreatorId !== book.publisherId) && (
-        <CardCreatorCard creator={book.publisher ?? null} />
+        <CardCreatorCard creator={book.artist ?? null} />
       )}
+      {showPublisher &&
+        (!currentCreatorId || currentCreatorId !== book.publisherId) && (
+          <CardCreatorCard creator={book.publisher ?? null} />
+        )}
     </div>
   );
 };
