6303f73028ae25a2aaa2db8076d075203187254d
Eanna Freeney <eannafreeney@gmail.com>
2026-04-21 19:01:39 +0200

    fixed creators

--- src/fs-routes/(app)/fragments/creators-slider.tsx
+++ src/fs-routes/(app)/fragments/creators-slider.tsx
@@ -11,14 +11,14 @@ export const GET = createRoute(async (c) => {
 
   return c.html(
     <div id="creators-slider-fragment" class="mt-2 mb-4 px-4">
-      <div class="flex items-center justify-between mb-2">
-        <SectionTitle>Popular Creators</SectionTitle>
-        <div class="flex items-center gap-2 ">
+      {/* <div class="flex items-center justify-between mb-2"> */}
+      <SectionTitle>Popular Creators</SectionTitle>
+      {/* <div class="flex items-center gap-2 ">
           <ViewAllLink href="/artists" text="Artists" />
           <span class="h-4 w-px bg-outline" aria-hidden="true"></span>
           <ViewAllLink href="/publishers" text="Publishers" />
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
       <CreatorsSlider creators={creators} />
     </div>,
   );
