b73c45ff282656a57ab2af9d6410e1d9528fdb8d
Eanna Freeney <eannafreeney@gmail.com>
2026-05-11 21:11:23 +0200

    working

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [
     tailwindcss(),
     devServer({
