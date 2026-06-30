a40763ce0ce028b32fd8e5b8f052332392c43203
Eanna Freeney <eannafreeney@gmail.com>
2026-06-30 12:48:33 +0200

    added publish button to interviews

--- src/features/dashboard/admin/interviews/client/editInterviewForm.tsx
+++ src/features/dashboard/admin/interviews/client/editInterviewForm.tsx
@@ -1,7 +1,6 @@
 import Alpine from "alpinejs";
 import {
   handleSubmit,
-  getIsDirty,
   createFormState,
   validateField,
   initFormValues,
@@ -29,20 +28,14 @@ export function registerEditInterviewForm() {
           return validateField(this, field, interviewFormSchema);
         },
 
-        get isDirty() {
-          return getIsDirty(this, INTERVIEW_FORM_FIELDS);
-        },
-
         get isFormValid() {
           const ctx = this as unknown as {
             errors: {
               form: Record<keyof typeof interviewFormSchema.shape, string>;
             };
             form: typeof interviewFormSchema.shape;
-            isDirty: boolean;
           };
           return (
-            ctx.isDirty &&
             Object.values(ctx.errors.form).every((err) => !err) &&
             ctx.form.q1 &&
             ctx.form.q2 &&
